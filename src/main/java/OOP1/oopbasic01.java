e78cc7f298c3e27cc6fd90798744b7bbdf1b0f01
Lim_Junyeop <skystom2@naver.com>
2022-05-10 21:32:28 +0900

    Basic 05

--- src/main/java/OOP1/oopbasic01.java
+++ src/main/java/OOP1/oopbasic01.java
@@ -38,8 +38,8 @@ package OOP1;
     //연관된 logic 들이 반복되서 나온다.
     //여기서 a,b 같은 "변수(data)"와 sum,avg 같은 "method" 들을 Grouping 해보자 => Object-Oriented Programming
 */
-/*
-    class calculator{
+/*    //객체 지향 - calculator 객체 생성
+    class calculator{                              //클래스 생성
         int a, b;
         public void set(int a, int b){
             this.a = a;
@@ -56,7 +56,7 @@ package OOP1;
 
     public class oopbasic01 {
         public static void main(String[] args) {
-            calculator c1 = new calculator();
+            calculator c1 = new calculator();                   // instance 생성
             c1.set(10,20);
             c1.sum();
             c1.avg();
@@ -90,7 +90,7 @@ package OOP1;
     }
      //-------------------------------------------- class 설계도 ---------3개의 method 를 정의함 ------------
     public class oopbasic01{
-        public static void main(String[] args) {               //평범한 class 시작
+        public static void main(String[] args) {               //평범한 main 시작
             caca c1 = new caca();                              //위에서 만든 객체 caca 사용하겠다
             c1.set(10,50);                                     //c1 <= 객체 caca 의 구체적 모습 = instance
             c1.sum();                                          //거기다가 위에서 정의했던 method 들 사용하겠다
@@ -101,8 +101,8 @@ package OOP1;
 */
 /*
         class calculator {
-        static int a = 1;
-        int b = 2;
+        static int a = 1;          //class variable
+        int b = 2;                 //instance variable
 
 
 
@@ -125,8 +125,9 @@ package OOP1;
 
                 calculator c1 = new calculator();
                 c1.static_static();
+              //c1.static_instance();                    // class method -> instance 변수 (x) 생성 안됨
                 c1.instance_static();
-                c1.instance_instance();                   // class method -> instance 변수 (x) 생성 안됨
+                c1.instance_instance();
 
                 calculator.static_static();               //class 로 직접접근 -> instance 는 아직 생성이 안됬으므로
                                                           // class - class 만 가능
@@ -188,15 +189,20 @@ package OOP1;
             System.out.println(title);
         }
          }                                                       //성공
+         //주의! 하지만 전역변수를 쓰는건 상당히 위험하다.
+         //특히 method 에서는 전역변수가 적용되게 하는건 매우 바람직하지 못함 (위험성, 재활용성, 분리하기 쉽게끔 해야해)
+         //method 는 parameter, arguments 인자값으로 작동하게 하는걸 거의 원칙으로 하자
+
 */
+           //static scope(lexical scope)
 /*      public class oopbasic01{
           static int i = 5;
           static void a(){
               int i = 10;
-              b();
+              b();                                //메소드 안에 메소드 넣어서 연쇄적으로 실행
           }
           static void b(){
-              //int i = 30;
+              //int i = 30;                       // 지역변수>전역변수 (우선순위가 더 높다)
               System.out.println(i);              // 정적 유효범위 (static scope)
           }                                       // 1.자기 자신범위(30) 2.전역변수(5)
 
@@ -206,17 +212,358 @@ package OOP1;
     }
 */
 
+       // this. -> instance 자신, 객체의 전역을 의미
+/*
+         class scope{
+             int a = 10;
+         void m(){
+             int a = 20;                                  //기본 전역 가져다 씀, 예외 줄때 지역 따로줘서 쓰는 방식
+             System.out.println(a);
+             System.out.println(this.a);                  //지역 가져다 쓰는 메소드에서, 특정부분은 전역을 쓰고자 할때
+         }                                                // this. 으로 전역꺼 가져다 쓸 수 있음
+         }
+         public class oopbasic01{
+             public static void main(String[] args) {
+                 scope c1 = new scope();
+                 c1.m();
+             }                                        //java 에서 this. 안붙여도 지역에 없는 변수값은 자동으로
+         }                                             //전역으로 가서 찾아서 씀.
+*/
+
+//         생성자 Constructor
+/*
+     class calculator{
+         int a, b;
+         public calculator(int c, int d){                          // 생성자 (class 와 같은 이름. instance 를 생성)
+             this.a = c;
+             this.b = d;
+         }
+         public void sum(){
+             System.out.println(this.a + this.b);
+         }
+         public void multi(){
+             System.out.println(this.a*this.b);
+         }
+     }
+     public class oopbasic01{
+         public static void main(String[] args) {
+             calculator c1 = new calculator(10, 20);
+             c1.sum();                                 //(class)calculator c1 = new (constructor)calculator(a,b);
+             c1.multi();
+
+             calculator c2 = new calculator(20, 40);
+             c2.sum();
+             c2.multi();
+         }
+     }
+*/
+       // 상속
+ //calculator 라는 상위 클래스를 subtract method 를 추가한 하위 클래스가 "상속"한다.
+
+/*
+class calculator{
+    int a, b;
+
+    public void set(int c, int d){
+        this.a = c;
+        this.b = d;
+    }
+    public void sum(){
+        System.out.println(this.a + this.b);
+    }
+    public void multi(){
+        System.out.println(this.a*this.b);
+    }
+}
+
+//하위 class 추가
+class upgraded_calculator extends calculator{
+    public void subtract(){
+        System.out.println(this.a - this.b);
+    }
+
+}
+public class oopbasic01{
+    public static void main(String[] args) {
+        upgraded_calculator c1 = new upgraded_calculator();
+        c1.set(10,20);
+        c1.sum();
+        c1.multi();
+        c1.subtract();
+
+        upgraded_calculator c2 = new upgraded_calculator();
+        c2.set(20,40);
+        c2.sum();
+        c2.multi();
+        c2.subtract();
+    }
+}
+*/                                  //상속자 있는 상위 class 에 extend 할때 오류났다. 보충 필요
+/*
+// 상속 두번 해보자.
+class calculator{
+    int a, b;
+
+    public void set(int c, int d){
+        this.a = c;
+        this.b = d;
+    }
+    public void sum(){
+        System.out.println(this.a + this.b);
+    }
+    public void multi(){
+        System.out.println(this.a*this.b);
+    }
+}
+
+//하위 class 추가
+class upgraded_calculator extends calculator{
+    public void subtract(){
+        System.out.println(this.a - this.b);
+    }
+
+}
+//하위 class 하나 더 추가 -> 하위 class를 상속하는 더 하위 class
+class calculator2 extends upgraded_calculator{
+    public void avg(){
+        System.out.println((this.a+this.b)/2);
+    }
 
 
+}
+public class oopbasic01{
+    public static void main(String[] args) {
+        calculator2 c1 = new calculator2();
+        c1.set(10,20);
+        c1.sum();
+        c1.multi();
+        c1.subtract();
+        c1.avg();
+
+        calculator2 c2 = new calculator2();
+        c2.set(20,40);
+        c2.sum();
+        c2.multi();
+        c2.subtract();
+        c2.avg();
+    }
+}
+*/
+/*     //public class 자체를 main 에서 스스로 instance 로 만드는 것 가능
+      public class oopbasic01{
+          public static void main(String[] args) {
+              oopbasic01 c1 = new oopbasic01();
+          }
+     }
+*/
+/*            //생성자가 있을 때
+      public class oopbasic01{
+          public oopbasic01(){}
+          public oopbasic01(int a){}          //기본 생성자가 아님. parameter 갖는 생성자.
+                                              // 인자값을 줘야 하는 생성자. -> 밑에서는 인자값이 없으므로 error
+                                              // 그러므로 명시적으로 기본생성자(parameter 없는)를 선언해주면
+                                              // instance 는 거기로 들어가며 정의됨.
+                                              // 밑에서 인자값을 주면 int a 라는 parameter 로 들어가며 정의됨.
+    public static void main(String[] args) {
+        oopbasic01 c1 = new oopbasic01();
+    }
+}
+
+*/
+        //생성자 있을때 상속
+/*
+class calculator{
+                                 //super class 에 생성자 만들면 -> 상속할때 ERROR 발생
+                                          // "기본생성자" 가 없기 때문. 명시적으로 선언해주면 된다.
+    int a, b;
+    public void sum(){
+        System.out.println(this.a + this.b);
+    }
+    public void multi(){
+        System.out.println(this.a*this.b);
+    }
+}
+class calculator2 extends calculator{
+    public calculator2(int left, int right){         //sub class 에서 생성자만들어야 함
+        this.a = left;                               //instance 정의할때도 sub class 로 정의하잖아
+        this.b = right;
+    }
+    public void subtract(){
+        System.out.println(this.a - this.b);
+    }
+}
+public class oopbasic01{
+    public static void main(String[] args) {
+        calculator2 c1 = new calculator2(10, 20);
+        c1.sum();
+        c1.multi();
+        c1.subtract();
+
+        calculator2 c2 = new calculator2(20, 40);
+        c2.sum();
+        c2.multi();
+        c2.subtract();
+    }
+}
+*/
+       //super(int a, int b); < 로 super class 생성자 땡겨쓰기
+/*
+class calculator{
+int a, b;
+public calculator(int c, int d){
+    this.a = c;
+    this.b = d;
+}
+public void sum(){
+        System.out.println(this.a + this.b);
+        }
+public void multi(){
+        System.out.println(this.a*this.b);
+        }
+        }
+class calculator2 extends calculator{
+    public calculator2(int i, int f){
+        super(i, f);
+    }
+    public void subtract(){
+        System.out.println(this.a - this.b);
+    }
+}
+public class oopbasic01{
+    public static void main(String[] args) {
+        calculator2 c1 = new calculator2(10, 20);
+        c1.sum();
+        c1.multi();
+        c1.subtract();
+
+        calculator2 c2 = new calculator2(20, 40);
+        c2.sum();
+        c2.multi();
+        c2.subtract();
+    }
+}
+*/
+
+// 생성자 - 상속 결론
+// 1. super class 에서 생성자 만들고, sub class 에서 super 로 땡겨쓰거나,
+// 2. super class 에서는 기본생성자만 주고(안만들고), sub class 에서 생성자 만들어 쓰기
+
+       // overriding - 생성자 있건없건 된다.
+/*
+class calculator{
+    int a, b;
+    public calculator(int c, int d){
+        this.a = c;
+        this.b = d;
+    }
+    public void sum(){
+        System.out.println(this.a + this.b);
+    }
+    public void multi(){
+        System.out.println(this.a*this.b);
+    }
+}
+class calculator2 extends calculator{
+    public calculator2(int i, int f){
+        super(i, f);
+    }
+    public void sum(){                                              // overriding
+        System.out.println("결과는"+ (this.a + this.b) + "입니다.");
+
+    }
+    public void subtract(){
+        System.out.println(this.a - this.b);
+    }
+}
+public class oopbasic01{
+    public static void main(String[] args) {
+        calculator2 c1 = new calculator2(10, 20);
+        c1.sum();
+        c1.multi();
+        c1.subtract();
+
+        calculator2 c2 = new calculator2(20, 40);
+        c2.sum();
+        c2.multi();
+        c2.subtract();
+    }
+}
+*/
+         //print 로 된걸 return 으로 overriding 하고 싶을때는 그냥 둘다 return 으로 해야한다.
+         // 한쪽만 print 로 하면 error 난다.
+         // 그럼 사실상 그냥 super 에만 method 선언하고 그대로 상속 하면 되겠지만,
+         // 굳이 sub 에서 overriding 을 하자면, super.method 이용해서 중복 없애주면 된다.
+/*
+          class calculator{
+           int a, b;
+           public calculator(int c, int d){
+               this.a = c;
+               this.b = d;
+           }
+           public int sum(){
+               return (this.a + this.b);
+
+           }
+           public void multi(){
+               System.out.println(this.a*this.b);
+           }
+       }
+class calculator2 extends calculator{
+    public calculator2(int i, int f){
+        super(i, f);
+    }
+    public int sum(){
+        return (super.sum());
+    }
+
+    public void subtract(){
+        System.out.println(this.a - this.b);
+    }
+}
+public class oopbasic01{
+    public static void main(String[] args) {
+        calculator2 c1 = new calculator2(10, 20);
+        c1.sum();
+        c1.multi();
+        c1.subtract();
+
+        calculator2 c2 = new calculator2(20, 40);
+        c2.sum();
+        c2.multi();
+        c2.subtract();
+    }
+}
+*/
+/*
+    class cal{
+        int a, b;
+        int c = 0;              // sum 에서는 this.c 더해야하므로 a,b만 있을 때 c 값을 전역변수에서 0 으로 선언해둔다.
+                                // 그냥 int c; 로만 해도 null 로 봐서 빼고 결과값이 나오긴 한다.
+    public void set(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+    public void set(int a, int b, int c){
+        this.a = a;                                 //여기 두줄 중복
+        this.b = b;                                 //this.set(a,b); 로 교환 가능
+        this.c = c;                                 //교환 해놔야 위에거에 종속되서 수정용이해짐
+    }
+
+    public void sum(){
+        System.out.println(this.a+this.b+this.c);
+    }
+    }
+    public class oopbasic01{
+        public static void main(String[] args) {
+            cal c1 = new cal();
+            c1.set(10, 20);
+            c1.sum();
+
+            c1.set(10, 20, 30);
+            c1.sum();
+        }
+    }
+*/
 
 
-
-
-
-
-
-
-
-
-//
 
---
+++ srcbin/classpathdemo.java
@@ -0,0 +1,9 @@
+class item2{
+public void printer(){System.out.println("Hello java");}
+}
+
+class path2{
+public static void main(String[] args){
+item2 c1 = new item2();
+c1.printer();}
+}
